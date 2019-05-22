6a8f50ea7c9acdf08d5103e1dc00446c51cdcccd
Noah Levin <50681582+nllevin@users.noreply.github.com>
2019-05-21 21:42:46 -0400

    Write basic PolyTreeNode methods

--- W1D5/PolyTreeNode/lib/00_tree_node.rb
+++ W1D5/PolyTreeNode/lib/00_tree_node.rb
@@ -1,3 +1,34 @@
 class PolyTreeNode
-    
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent
+        @parent
+    end
+
+    def children
+        @children
+    end
+
+    def value
+        @value
+    end
+
+    def parent=(parent_node)
+        @parent.children.delete(self) unless @parent.nil?
+        @parent = parent_node
+        parent_node.children << self unless parent_node.nil?
+    end
+
+    def add_child(child_node)
+        child_node.parent = self
+    end
+
+    def remove_child(child_node)
+        raise unless self.children.include?(child_node)
+        child_node.parent = nil
+    end
 end
