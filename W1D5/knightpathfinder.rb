a387d41c64dffd294f9e63715e60310618c4b5ab
Noah Levin <50681582+nllevin@users.noreply.github.com>
2019-05-21 22:54:20 -0400

    Track @considered_positions and find #new_move_positions(pos)

--- W1D5/knightpathfinder.rb
+++ W1D5/knightpathfinder.rb
@@ -1,8 +1,10 @@
 require "./PolyTreeNode/lib/00_tree_node.rb"
 
 class KnightPathFinder
+    attr_reader :considered_positions
     def initialize(start_pos)
         @root_node = PolyTreeNode.new(start_pos)
+        @considered_positions = [start_pos]
     end
 
     def self.valid_moves(pos)
@@ -22,5 +24,11 @@ class KnightPathFinder
             valids << [x+dx, y+dy] if (x+dx).between?(0,8) && (y+dy).between?(0,8)
         end
         valids
+    end
+
+    def new_move_positions(pos)
+        new_moves = KnightPathFinder.valid_moves(pos).reject { |valid_move| @considered_positions.include?(valid_move) }
+        @considered_positions.concat(new_moves)
+        new_moves
     end
 end
