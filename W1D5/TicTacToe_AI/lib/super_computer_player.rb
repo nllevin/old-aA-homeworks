0673b455bc143662fc9407a8b35e7ae7aaa2291e
Noah Levin <50681582+nllevin@users.noreply.github.com>
2019-05-22 19:31:41 -0400

    Write SuperComputerPlayer subclass; overwrite #move

--- W1D5/TicTacToe_AI/lib/super_computer_player.rb
+++ W1D5/TicTacToe_AI/lib/super_computer_player.rb
@@ -2,6 +2,16 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = TicTacToeNode.new(game.board, mark)
+    child_nodes = node.children
+
+    winning_child_nodes = child_nodes.select { |child| child.winning_node?(mark) }
+    return winning_child_nodes.sample.prev_move_pos unless winning_child_nodes.empty?
+    
+    non_losing_child_nodes = child_nodes.reject { |child| child.losing_node?(mark) }
+    return non_losing_child_nodes.sample.prev_move_pos unless non_losing_child_nodes.empty?
+
+    raise "Something went wrong: computer should always be able to force a draw."
   end
 end
 
