9269c623a769dd2fcf196c64813fe7566d8e7bbf
Noah Levin <50681582+nllevin@users.noreply.github.com>
2019-05-23 21:41:42 -0400

    Complete all aspects of user interaction

--- W2D1/Simon/lib/simon.rb
+++ W2D1/Simon/lib/simon.rb
@@ -9,8 +9,8 @@ class Simon
 
   def play
     self.take_turn until @game_over
-    self.game_over_message
     self.reset_game
+    self.game_over_message
   end
 
   def take_turn
@@ -24,10 +24,36 @@ class Simon
 
   def show_sequence
     self.add_random_color
+    
+    system("clear")
+    puts "Watch the sequence carefully:"
+    sleep(1)
+    system("clear")
+
+    @seq.each do |color|
+      sleep(0.1)
+      print color
+      sleep(0.8)
+      system("clear")
+    end
   end
 
   def require_sequence
+    @seq.each do |color|
+      puts "Enter the colors in the sequence in order:"
+      
+      user_input = gets.chomp.downcase
+      until COLORS.include?(user_input)
+        puts "Sorry, not a valid color choice. Try again:"
+        user_input = gets.chomp.downcase
+      end
+      system("clear")
 
+      unless color == user_input
+        @game_over = true
+        break
+      end
+    end
   end
 
   def add_random_color
@@ -35,11 +61,23 @@ class Simon
   end
 
   def round_success_message
-
+    puts "Great job, you passed that round!"
   end
 
   def game_over_message
+    puts "Uh oh, you messed up. Game over!"
 
+    user_input = nil
+    until ["y", "n"].include?(user_input)  
+      puts "Play again? (y/n)"
+      user_input = gets.chomp.downcase
+    end
+
+    if user_input == "y"
+      self.play
+    else
+      exit
+    end
   end
 
   def reset_game
