0f95663fa9e28a2a31839f23663b5d4c97e3c74b
Noah Levin <50681582+nllevin@users.noreply.github.com>
2019-05-24 13:25:18 -0400

    Raise and handle descriptive fruit monster and BestFriend errors

--- W2D1/error_handling_funtime/super_useful.rb
+++ W2D1/error_handling_funtime/super_useful.rb
@@ -7,26 +7,43 @@ end
 
 # PHASE 3
 FRUITS = ["apple", "banana", "orange"]
+class CoffeeError < ArgumentError
+end
 
 def reaction(maybe_fruit)
   if FRUITS.include? maybe_fruit
     puts "OMG, thanks so much for the #{maybe_fruit}!"
   else 
-    raise StandardError 
+    raise CoffeeError if maybe_fruit == "coffee"
+    raise ArgumentError 
   end 
 end
 
 def feed_me_a_fruit
   puts "Hello, I am a friendly monster. :)"
-
-  puts "Feed me a fruit! (Enter the name of a fruit:)"
-  maybe_fruit = gets.chomp
-  reaction(maybe_fruit) 
+  
+  begin
+    puts "Feed me a fruit! (Enter the name of a fruit:)"
+    maybe_fruit = gets.chomp
+    reaction(maybe_fruit)
+    rescue CoffeeError
+      puts "Well it's not what I asked for, but I do like coffee! :)"
+      retry
+    rescue ArgumentError
+      puts "Grrrrr that isn't fruit!"
+  end
 end  
 
 # PHASE 4
+class ShortFriendshipError < StandardError
+end
+
 class BestFriend
   def initialize(name, yrs_known, fav_pastime)
+    raise ArgumentError, "Best friend must have a name" if name.length <= 0
+    raise ShortFriendshipError, "Too short a time to be best friends (< 5 years)" if yrs_known < 5
+    raise ArgumentError, "Best friends must have a favorite pastime" if fav_pastime.length <= 0
+
     @name = name
     @yrs_known = yrs_known
     @fav_pastime = fav_pastime
