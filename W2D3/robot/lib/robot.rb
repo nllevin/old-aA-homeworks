0aa270be331e82949acaa7b3f2dc8ab1237d52af
Noah Levin <50681582+nllevin@users.noreply.github.com>
2019-06-09 23:48:42 -0400

    Satisfy other specs

--- W2D3/robot/lib/robot.rb
+++ W2D3/robot/lib/robot.rb
@@ -1,8 +1,9 @@
 class Robot
-    attr_reader :position
+    attr_reader :position, :items, :health
+    attr_accessor :equipped_weapon
 
     def initialize
-        @position = [0, 0]
+        @position, @items, @health = [0, 0], [], 100, nil
     end
 
     def move_left
@@ -20,21 +21,77 @@ class Robot
     def move_down
         @position = [ @position[0], @position[1] - 1 ]
     end
+
+    def pick_up(item)
+        if items_weight + item.weight <= 250
+            @items << item 
+        else
+            raise ArgumentError
+        end
+    end
+
+    def items_weight
+        @items.empty? ? 0 : @items.map(&:weight).sum
+    end
+
+    def wound(amount)
+        @health -= amount
+        @health = 0 if @health < 0
+    end
+
+    def heal(amount)
+        @health += amount
+        @health = 100 if @health > 100
+    end
+
+    def attack(other_robot)
+        if @equipped_weapon.nil? 
+            other_robot.wound(5)
+        else
+            @equipped_weapon.hit(other_robot)
+        end
+    end
 end
 
 class Item
+    attr_reader :name, :weight
 
+    def initialize(name, weight)
+        @name, @weight = name, weight
+    end
 end
 
-class Bolts
+class Bolts < Item
+    def initialize
+        super("bolts", 25)
+    end
 
+    def feed(robot)
+        robot.heal(25)
+    end
 end
 
-class Weapon
+class Weapon < Item
+    attr_reader :damage
+
+    def initialize(name, weight, damage)
+        @damage = damage
+        super(name, weight)
+    end
+
+    def hit(robot)
+        robot.wound(damage)
+    end
 end
 
-class Laser
+class Laser < Weapon
+    def initialize
+        super("laser", 125, 25)
+    end
 end
 
-class PlasmaCannon
+class PlasmaCannon < Weapon
+    def initialize
+        super("plasma_cannon", 200, 55)
+    end
 end
