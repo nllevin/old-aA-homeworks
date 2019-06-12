da6ed39d71836643373298a7f549256f2c6d14d3
nllevin <50681582+nllevin@users.noreply.github.com>
2019-06-12 14:55:39 -0400

    Write specs for and implement #two_sum

--- W2D3/first_tdd_projects/lib/first_tdd_projects.rb
+++ W2D3/first_tdd_projects/lib/first_tdd_projects.rb
@@ -6,4 +6,14 @@ class Array
         end
         uniqs
     end
+
+    def two_sum
+        zero_sums = []
+        (0...self.count - 1).each do |idx_1|
+            (idx_1 + 1...self.count).each do |idx_2|
+                zero_sums << [idx_1, idx_2] if self[idx_1] + self[idx_2] == 0
+            end
+        end
+        zero_sums
+    end
 end
--- W2D3/first_tdd_projects/spec/first_tdd_projects_spec.rb
+++ W2D3/first_tdd_projects/spec/first_tdd_projects_spec.rb
@@ -21,3 +21,30 @@ describe '#my_uniq' do
         end
     end
 end
+
+describe '#two_sum' do
+    context 'when given an empty array' do
+        it 'returns an empty array' do
+            expect([].two_sum).to be_empty
+        end
+    end
+
+    context 'when no two elements sum to zero' do
+        it 'returns an empty array' do
+            expect([1, 2, 3, 4, 5].two_sum).to be_empty
+        end
+    end
+
+    context 'when some elements sum to zero' do
+        subject(:arr) { [-1, 0, 2, -2, 1] }
+
+        it 'returns pairs of positions for elements adding to zero' do
+            expect(arr.two_sum).to match_array([[2, 3], [0, 4]])
+        end
+
+        it 'is sorted "dictionary-wise"' do
+            expect(arr.two_sum).to eq([[0, 4], [2, 3]])
+        end
+    end
+
+end
